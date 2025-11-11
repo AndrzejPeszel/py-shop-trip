8b67e03694fa7c533f1c64ec7afd541bcda1d04c
AndrzejPeszel <andrzej.peszel@icloud.com>
2025-11-11 17:27:28 +0100

    fixed solution #2

--- app/shop.py
+++ app/shop.py
@@ -1,22 +1,25 @@
 from datetime import datetime
 
 class Shop:
-    def __init__(self, name, location, products):
+    def __init__(self, name: str, location: list[float], products: dict[str, float]):
         self.name = name
         self.location = location
         self.products = products
 
-    def can_fulfill(self, product_cart):
+    def can_fulfill(self, product_cart: dict[str, int]) -> bool:
         return all(product in self.products for product in product_cart)
 
-    def calculate_product_cost(self, product_cart):
-        return sum(self.products[product] * quantity for product, quantity in product_cart.items())
+    def calculate_product_cost(self, product_cart: dict[str, int]) -> float:
+        return sum(
+            self.products[product] * quantity
+            for product, quantity in product_cart.items()
+        )
 
-    def print_receipt(self, customer_name, product_cart):
+    def print_receipt(self, customer_name: str, product_cart: dict[str, int]) -> float:
         print(f"\nDate: {datetime.now().strftime('%m/%d/%Y %H:%M:%S')}")
         print(f"Thanks, {customer_name}, for your purchase!")
         print("You have bought:")
-        total = 0
+        total = 0.0
         for product, quantity in product_cart.items():
             price = self.products[product] * quantity
             print(f"{quantity} {product}s for {price:.2f} dollars")
